5eee3c5ce9c1e03142dc5263dae55af3b1f85afb
O-sulloc <bujjaf@gmail.com>
2022-10-04 15:23:21 +0900

    codeup

---
+++ src/evenOddAdd/Main.java
@@ -0,0 +1,33 @@
+package evenOddAdd;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        if (num % 2 != 0) {
+            System.out.print("홀수");
+        } else {
+            System.out.print("짝수");
+        }
+
+        System.out.print("+");
+
+        if (num2 % 2 != 0) {
+            System.out.print("홀수");
+        } else {
+            System.out.print("짝수");
+        }
+        System.out.print("=");
+
+        if ((num + num2) % 2 == 0) {
+            System.out.print("짝수");
+        }else {
+            System.out.print("홀수");
+        }
+    }
+}
