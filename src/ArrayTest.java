f3d78248cb7b599107e1b0fa898ceec6e990b8f6
O-sulloc <bujjaf@gmail.com>
2022-10-04 13:29:29 +0900

    arrayCommit

---
+++ src/ArrayTest.java
@@ -0,0 +1,12 @@
+public class ArrayTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        // 1부터 10까지 채우고 출력
+        int[] arr = new int[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(i+1);
+        }
+    }
+}
